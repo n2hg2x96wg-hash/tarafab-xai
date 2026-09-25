8a47142261c6eccc8a9b06f8b0d9607e16dd7ceb
n2hg2x96wg-hash <n2hg2x96wg@privaterelay.appleid.com>
2026-09-25 02:20:46 -0500

    Align router base path with deployment environment

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,10 @@ function AppRoutes() {
 }
 
 export default function App() {
+  const basename = import.meta.env.BASE_URL.replace(/\/$/, '');
+
   return (
-    <Router>
+    <Router basename={basename}>
       <ToastProvider>
         <AdminAuthProvider>
           <AuthProvider>
