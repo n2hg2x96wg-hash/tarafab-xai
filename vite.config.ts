56fefe4a347c662b706f01f91551922881eb1130
n2hg2x96wg-hash <n2hg2x96wg@privaterelay.appleid.com>
2026-09-25 02:19:15 -0500

    Configure GitHub Pages project base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isGitHubPagesBuild = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
+  base: isGitHubPagesBuild ? '/tarafab-xai/' : '/',
   plugins: [react()],
   server: {
     proxy: {
